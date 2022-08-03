2694c5d0c9ceef1bfe0d8f44547410485847392d
Ryan Sy <ryansy@HP400.mynetworksettings.com>
2022-08-03 14:57:15 -0400

    login/logout done

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AuthModule } from '@auth0/auth0-angular';
+
+import { LoginComponent } from 'src/app/components/login/login.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-tosc339w.auth0.com',
+      clientId: 'VcOPxm1ZUm8IjWaXhakX1sZ1lfk43GtY'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
