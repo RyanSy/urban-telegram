53e44a75828fbb10da7cbc4edb3c64e71a401c03
bpaser <60895408+bpaser@users.noreply.github.com>
2022-08-03 11:43:50 -0500

    pushing up the backend

---
+++ BackEnd/Urban-telegram/src/main/java/com/example/Urbantelegram/UrbanTelegramApplication.java
@@ -0,0 +1,13 @@
+package com.example.Urbantelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanTelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanTelegramApplication.class, args);
+	}
+
+}
---
+++ BackEnd/Urban-telegram/src/test/java/com/example/Urbantelegram/UrbanTelegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Urbantelegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanTelegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
