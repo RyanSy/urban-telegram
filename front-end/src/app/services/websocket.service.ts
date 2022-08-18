ac1c8935e38f5057714cbad8dd83436d87cdf430
bpaser <60895408+bpaser@users.noreply.github.com>
2022-08-18 10:40:17 -0500

    added websocket service for websocket connection

---
+++ front-end/src/app/services/websocket.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import * as SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebsocketService {
+
+  constructor() { }
+
+  public stompClient;
+  public msg = [];
+
+  initializeWebSocketConnection(roomId: string) {
+    const serverUrl = 'http://localhost:8081/socket';
+    const ws = new SockJS(serverUrl);
+    this.stompClient = Stomp.over(ws);
+    const that = this;
+    
+    this.stompClient.connect({}, function(frame) {
+      that.stompClient.subscribe('/message/' + roomId, (message) => {
+        if(message.body) {
+          that.msg.push(message.body);
+        }
+      });
+    });
+  }
+
+}
